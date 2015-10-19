d57e2083be887883cc596228c305608e15bb9f83
doomsterinc <doomsterinc1@gmail.com>
2015-10-19 20:32:06 -0200

    testejs

---
+++ mirrorfashion/js/home.js
@@ -0,0 +1,19 @@
+function validaBusca() {
+	if (document.querySelector('#q').value ===''){
+		alert('Campo Vazio!');
+		return false;	
+	}
+}
+
+// fazendo a associação da função como evento
+document.querySelector('#form-busca').onsubmit = validaBusca;
+
+var banners = ["img/destaque-home.png", "img/destaque-home-2.png"];
+var bannerAtual=0;
+
+function trocaBanner() {
+	bannerAtual = (bannerAtual + 1) % 2;
+	document.querySelector('.destaque img').src = banners[bannerAtual];
+}
+
+setInterval(trocaBanner, 4000);
---
+++ mirrorfashion/js/testes.js
@@ -0,0 +1,10 @@
+for (var i=0; i<10; i++){
+	if (i%2==0){
+		console.log(i);
+		console.log("esse numero é par");
+	}
+	else{
+		console.log(i);
+		console.log("esse numero e impar");	
+	}
+}
