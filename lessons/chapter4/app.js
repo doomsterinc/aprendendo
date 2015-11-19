a1f65832960a1c9635720353040792b4fd0278ca
doomsterinc <doomsterinc1@gmail.com>
2015-11-19 00:36:49 -0200

    mdp12

--- lessons/chapter4/app.js
+++ lessons/chapter4/app.js
@@ -1,9 +1,25 @@
 var main = function () {
 	"use strict";
+	var addCommentFromInputInbox = function () {
+		var $new_comment;
+		if($(".comment-input input").val() !== ""){		
+			$new_comment = $("<p>").text($(".comment-input input").val());
+			$new_comment.hide();
+			$(".comments").append($new_comment);
+			$new_comment.fadeIn();
+			$(".comment-input input").val("")
+		}
+	}
 
 	$(".comment-input button").on("click", function (event){
-		var $new_comment = $("<p>").text($(".comment-input input").val());
-		$(".comments").append($new_comment);	
+		addCommentFromInputInbox();	
+	});
+	
+
+	$(".comment-input input").on("keypress", function (event){
+		if(event.keyCode === 13){
+			addCommentFromInputInbox();	
+		}	
 	});
 
 };
