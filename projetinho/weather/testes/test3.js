75a02b2c37ec8a0f9c4d7a8865efb62c9de262d3
doomsterinc <doomsterinc1@gmail.com>
2015-11-30 12:03:11 -0200

    alt1

---
+++ projetinho/weather/testes/test3.js
@@ -0,0 +1,5 @@
+var app = angular.module('myApp', []);
+app.controller('myCtrl', function($scope) {
+	$scope.firstNum = 0;
+	$scope.lastNum = 0;
+});
--- projetinho/weather/www/js/app.js
+++ projetinho/weather/www/js/app.js
@@ -1,19 +1,57 @@
-// Ionic Starter App
+(function() {
+    angular.module('weather', ['ionic', 'ngCordova'])
+        .run(function($ionicPlatform) {
+            $ionicPlatform.ready(function() {
+                // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+                // for form inputs)
+                if (window.cordova && window.cordova.plugins.Keyboard) {
+                    cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+                }
+                if (window.StatusBar) {
+                    StatusBar.styleDefault();
+                }
+            });
+        })
+        .factory('Weather', function($q, $http) {
+            var deferred = $q.defer();
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+            function getCurrentWeather(lat, lng) {
+                var url = 'https://api.forecast.io/forecast/d4e926a10d6198f2bb1d7a76e2469385/' + lat + ',' + lng + '?callback=JSON_CALLBACK';
+                $http.jsonp(url)
+                    .success(deferred.resolve)
+                    .error(deferred.reject);
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
-})
+                return deferred.promise;
+            }
+
+            return {
+                getCurrentWeather: getCurrentWeather
+            };
+        })
+        .controller('WeatherCtrl', function($scope, $cordovaGeolocation, Weather) {
+            $scope.loading = true;
+
+            $scope.toCelsius = function(temperature) {
+                return ((temperature - 32) / 1.8).toFixed(1);
+            };
+
+            $cordovaGeolocation
+                .getCurrentPosition({
+                    timeout: 10000,
+                    enableHighAccuracy: false
+                })
+                .then(function(position) {
+                    var lat = position.coords.latitude;
+                    var long = position.coords.longitude;
+
+                    Weather.getCurrentWeather(lat, long).then(function(data) {
+                        $scope.weatherInfo = data;
+                        $scope.loading = false;
+                    }, function(error) {
+                        //TODO Display error message
+                    });
+                }, function(err) {
+                    //TODO Display error message
+                });
+        });
+})();
