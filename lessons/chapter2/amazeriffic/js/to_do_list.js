897ec391977215b5347b7fa360cf17c50998d924
doomsterinc <doomsterinc1@gmail.com>
2015-12-01 00:45:12 -0200

    alt6

--- lessons/chapter2/amazeriffic/js/to_do_list.js
+++ lessons/chapter2/amazeriffic/js/to_do_list.js
@@ -1,38 +1,15 @@
 var main = function () {
 	"use strict";
-
-	var makeTabActive = function(tabNumber){
-		//controi o seletor apartir do tabNumber
-		var tabSeletor = ".tabs a:nth-child("+ tabNumber +") span"
-		//deixa todas as tabs inativas
-		$(".tabs span").removeClass("active");
-		
-		//deixa a primeira tab ativa
-		$(tabSeletor).addClass("active");
-
-		//esvazia oconteudo principalpara podermos recria-lo
-		$("main .content").empty();
-		
-	};
 	
-	$(".tabs a:nth-child(1)").on("click",function() {
-		makeTabActive(1);
-		//retorna false para nao seguirmos o link
-		return false;
-	});
-	
-	$(".tabs a:nth-child(2)").on("click", function() {
-		makeTabActive(2);
-		//retorna false para nao seguirmos o link
-		return false;
-	});
-	
-	$(".tabs a:nth-child(3)").on("click",function() {
-		makeTabActive(3);
-		//retorna false para nao seguirmos o link
-		return false;
-	});
- 
+	$(".tabs span").toArray().forEach(function(element) {
+		//cria um handler click para este elemento
+		$(element).on("click", function () {
+			$(".tabs span").removeClass("active");
+			$(element).addClass("active");
+			$("main .content").empty();
+			return false;
+		});	
+	}); 
 };
 
 $(document).ready(main);
