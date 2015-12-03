5957cbdacdddbaa423947133360cb5c33c2fc62c
doomsterinc <doomsterinc1@gmail.com>
2015-12-03 00:17:35 -0200

    alt13

--- lessons/chapter2/amazeriffic/js/to_do_list.js
+++ lessons/chapter2/amazeriffic/js/to_do_list.js
@@ -4,9 +4,23 @@ var main = function () {
 	$(".tabs span").toArray().forEach(function(element) {
 		//cria um handler click para este elemento
 		$(element).on("click", function () {
+			//variavel temporaria
+			var $element = $(element);
+
 			$(".tabs span").removeClass("active");
 			$(element).addClass("active");
 			$("main .content").empty();
+			
+			if ($element.parent().is(":nth-child(1)")) {
+				console.log("FIRST TAB CLICKED");
+			}			
+			else if ($element.parent().is(":nth-child(2)")) {
+				console.log("SECOND TAB CLICKED");
+			}
+			else if ($element.parent().is(":nth-child(3)")) {
+				console.log("THIRD TAB CLICKED");
+			}
+			
 			return false;
 		});	
 	}); 
