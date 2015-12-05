c14f8a9f18f63ee28f055a11cb4ed049b57c1a9e
doomsterinc <doomsterinc1@gmail.com>
2015-12-05 01:24:27 -0200

    alt19

--- projetinho/weather/testes/app.js
+++ projetinho/weather/testes/app.js
@@ -1,6 +1,23 @@
 var main = function() {
 	"use strict";
-	console.log("hello World!!!");
+	$("*").css("color", "red");
+	$(".important").css("color","green");
+	$("p:first").css("color","blue");
+	var i;
+	for (i = 0; i <= 100; i++) {
+		if ((i%3===0) && (i%5===0)){
+			console.log("FizzBuzz");		
+		}
+		else if ((i%3===0) && (i%5!==0)){
+			console.log("Fizz");
+		}
+		else if ((i%3!==0) && (i%5===0)){
+			console.log("Buzz");
+		}
+		else {
+			console.log(i);		
+		}
+	}
 };
 
 $(document).ready(main);
