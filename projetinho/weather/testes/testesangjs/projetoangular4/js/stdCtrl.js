6797d7fc119fd5182d6fd361c937dbc61d49efac
doomsterinc <doomsterinc1@gmail.com>
2015-12-15 10:58:43 -0200

    alt95

--- projetinho/weather/testes/testesangjs/projetoangular4/js/stdCtrl.js
+++ projetinho/weather/testes/testesangjs/projetoangular4/js/stdCtrl.js
@@ -1,20 +1,21 @@
-angular.module("mainApp").controller('stdCtrl', function($scope){
-	$scope.student = {
-		firstName = " ",
-		lastName = " ",
-
-		subjects = [
-			{name: 'Physhics', marks:70},
-			{name: 'Chemistry', marks:80},
-			{name: 'Math', marks:65},
-			{name: 'English', marks:75},
-			{name: 'Hindi', marks:67}
-		],
-
-		fullName : function() {
-			var studentObject;
-			studentObject = $scope.student;
-			return studentObject.firstName + " " + studentObject.lastName;
-		}
-	}
+mainApp.controller("stdCtrl", function($scope) {
+   $scope.student = {
+      firstName: "Mahesh",
+      lastName: "Parashar",
+      fees:500,
+      
+      subjects:[
+         {name:'Physics',marks:70},
+         {name:'Chemistry',marks:80},
+         {name:'Math',marks:65},
+         {name:'English',marks:75},
+         {name:'Hindi',marks:67}
+      ],
+      
+      fullName: function() {
+         var studentObject;
+         studentObject = $scope.student;
+         return studentObject.firstName + " " + studentObject.lastName;
+      }
+   };
 });
