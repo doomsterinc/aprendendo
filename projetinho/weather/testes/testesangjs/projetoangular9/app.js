41c09278c1c61b6d211b6525d8383bdc5e2d6c73
doomsterinc <doomsterinc1@gmail.com>
2015-12-16 20:18:52 -0200

    alt106

--- projetinho/weather/testes/testesangjs/projetoangular9/app.js
+++ projetinho/weather/testes/testesangjs/projetoangular9/app.js
@@ -14,3 +14,33 @@ myApp.directive('fileModel', ['$parse', function ($parse) {
 		}
 	};
 }]);
+
+myApp.service('fileUpload', ['$http', function() {
+	this.uploadFileToUrl = function(file, uploadUrl) {
+		var fd = new FormData();
+		fd.append('file', file);
+
+		$http.post(uploadUrl, fd, {
+			transformRequest : angular.identity,
+			headers : {'Content-Type': undefined}
+		})
+
+		.success(function(){		
+		})
+
+		.error(function(){
+		})
+	}
+}]);
+
+myApp.controller('myCtrl', ['$scope', 'fileUpload', function($scope, fileUpload) {
+	$scope.uploadFile = function(){
+		var file = $scope.myFile;
+
+		console.log("file is ");
+		console.dir(file);
+
+		var uploadUrl = "/fileUpload";
+		fileUpload.uploadFileToUrl(file,uploadUrl);
+	}
+}]);
