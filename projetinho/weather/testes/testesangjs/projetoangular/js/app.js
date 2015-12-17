cab0f3d1c07dc3bc78a8072daf9678a98836c454
doomsterinc <doomsterinc1@gmail.com>
2015-12-17 16:47:07 -0200

    alt114

--- projetinho/weather/testes/testesangjs/projetoangular/js/app.js
+++ projetinho/weather/testes/testesangjs/projetoangular/js/app.js
@@ -2,4 +2,18 @@ angular
 	.module("listaTelefonica", [])
 	.controller('listaTelefonicaCtrl', function($scope) {
 			$scope.app = "Lista Telefonica";
+			$scope.contatos = [
+				{nome: "Paulo", telefone : "99889988" },
+				{nome: "Henrique", telefone : "99889988" },
+				{nome: "Paul", telefone : "99889988" },
+				{nome: "Johnes", telefone : "99889988" },
+				{nome: "Abbath", telefone : "99889988" },
+				{nome: "Rafael", telefone : "99889988" },
+				{nome: "Rogerio", telefone : "99889988" },
+				{nome: "Sergio", telefone : "99889988" },
+				{nome: "Augusto", telefone : "99889988" },
+				{nome: "Pedro", telefone : "99889988" },
+				{nome: "Jaoa", telefone : "99889988" }
+
+			];
 	});
