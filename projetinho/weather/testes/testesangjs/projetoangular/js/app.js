15cd2bc90eacdab393106af3b6173251e6be1386
doomsterinc <doomsterinc1@gmail.com>
2015-12-21 15:02:07 -0200

    altr4

--- projetinho/weather/testes/testesangjs/projetoangular/js/app.js
+++ projetinho/weather/testes/testesangjs/projetoangular/js/app.js
@@ -4,17 +4,17 @@ angular
 			$scope.app = "Lista Telefonica";
 			
 			$scope.contatos = [
-				{nome: "Paulo", telefone : "99889988" },
-				{nome: "Henrique", telefone : "99889988" },
-				{nome: "Paul", telefone : "99889988" },
-				{nome: "Johnes", telefone : "99889988" },
-				{nome: "Abbath", telefone : "99889988" },
-				{nome: "Rafael", telefone : "99889988" },
-				{nome: "Rogerio", telefone : "99889988" },
-				{nome: "Sergio", telefone : "99889988" },
-				{nome: "Augusto", telefone : "99889988" },
-				{nome: "Pedro", telefone : "99889988" },
-				{nome: "Joao", telefone : "99889988" }
+				{nome: "Paulo", telefone : "99889988", cor: "blue" },
+				{nome: "Henrique", telefone : "99889988", cor: "blue" },
+				{nome: "Paul", telefone : "99889988", cor: "blue" },
+				{nome: "Johnes", telefone : "99889988", cor: "yellow" },
+				{nome: "Abbath", telefone : "99889988", cor: "black" },
+				{nome: "Rafael", telefone : "99889988", cor: "grey" },
+				{nome: "Rogerio", telefone : "99889988", cor: "white" },
+				{nome: "Sergio", telefone : "99889988", cor: "grey" },
+				{nome: "Augusto", telefone : "99889988", cor: "green" },
+				{nome: "Pedro", telefone : "99889988", cor: "black" },
+				{nome: "Joao", telefone : "99889988", cor: "yellow" }
 
 			];
 
@@ -40,4 +40,9 @@ angular
 				});
 			};
 
+			$scope.isContatoSelecionado = function (contatos) {
+				return contatos.some(function (contato) {
+					return contato.selecionado;
+				});			
+			};
 	});
