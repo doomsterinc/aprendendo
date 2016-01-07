0c2646ce596a23515676c0cbd699a6b2e74d1168
doomsterinc <doomsterinc1@gmail.com>
2016-01-07 20:19:44 -0200

    modFilterEllipsis

--- projetinho/weather/testes/testesangjs/projetoangular/backend/public/js/filters/ellipsisFilter.js
+++ projetinho/weather/testes/testesangjs/projetoangular/backend/public/js/filters/ellipsisFilter.js
@@ -1,7 +1,7 @@
 angular.module("listaTelefonica").filter("ellipsis", function () {
 	return function (input, size) {
 		if (input.length > 8){
-			var output = input.substring(0, size) + "...";
+			var output = input.substring(0, (size || 5)) + "...";
 			return (output);
 		} else {
 			return input;
