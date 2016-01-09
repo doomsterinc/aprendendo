f0c1e4f372fec810eec613c8844f3e24d0ad1520
doomsterinc <doomsterinc1@gmail.com>
2016-01-09 12:26:01 -0200

    modDirectiveAccordion

--- projetinho/weather/testes/testesangjs/projetoangular/backend/public/js/directives/uiAccordionDirective.js
+++ projetinho/weather/testes/testesangjs/projetoangular/backend/public/js/directives/uiAccordionDirective.js
@@ -1,3 +1,22 @@
+angular.module("listaTelefonica").directive("uiAccordions", function () {
+	return {
+		controller: function ($scope, $element, $attrs){
+			var accordions = [];
+
+			this.registerAccordions = function (accordions) {
+				accordions.push(accordion);
+			};
+
+			this.closeAll = function () {
+				accordions.forEach(function (accordion) {
+					accordion.isOpened = false;
+				});
+			};
+
+		}
+	};
+});
+
 angular.module("listaTelefonica").directive("uiAccordion", function () {
 	return {
 		templateUrl:"../../views/accordion.html",
@@ -5,9 +24,12 @@ angular.module("listaTelefonica").directive("uiAccordion", function () {
 		scope : {
 			title: "@"
 		},
+		require: "^uiAccordions",
 		link: function (scope, element, attrs, ctrl) {
+			ctrl.registerAccordions(scope);
 			scope.open = function () {
-				scope.isOpened = !scope.isOpened;
+				ctrl.closeAll();
+				scope.isOpened = true;
 			};
 		}
 	};
