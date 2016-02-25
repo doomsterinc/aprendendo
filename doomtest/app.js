e3b623b3fe1a9be100ba2298328cbf7452c8d75d
doomsterinc <doomsterinc1@gmail.com>
2016-02-25 16:30:42 -0300

    addProject

--- doomtest/app.js
+++ doomtest/app.js
@@ -1,3 +1,13 @@
-function ligar() {
-  document.getElementById("img_lamp").src("img/lamp_on.png");
+function changeLamp() {
+  var image = document.getElementById("img_lamp");
+  if(image.src.match("bulbon")) {
+  	image.src = "img/pic_bulboff.gif";
+  }
+  else {
+  	image.src = "img/pic_bulbon.gif";
+  }
+}
+
+function modify(){
+	document.getElementById("demo").innerHTML = "hello";
 }
