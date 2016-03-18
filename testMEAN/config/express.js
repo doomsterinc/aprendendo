188c7862d9a8bf8ffb3beed3e4f8cf7115d65dce
doomsterinc <doomsterinc1@gmail.com>
2016-03-18 17:03:00 -0300

    addmod021803

--- testMEAN/config/express.js
+++ testMEAN/config/express.js
@@ -2,5 +2,9 @@ var express = require("express");
 
 module.exports = function () {
 	var app = express();
+	//config ambiente
+	app.set('port', 3000);
+	//middleware
+	app.use(express.static('./public'));
 	return app;
 };
