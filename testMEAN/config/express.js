7a2945c5933333ab847d9b8ed7c198358e47eb24
doomsterinc <doomsterinc1@gmail.com>
2016-03-18 17:28:13 -0300

    addmod031803

--- testMEAN/config/express.js
+++ testMEAN/config/express.js
@@ -1,4 +1,5 @@
 var express = require("express");
+var home = require('../app/routes/home')
 
 module.exports = function () {
 	var app = express();
@@ -6,5 +7,8 @@ module.exports = function () {
 	app.set('port', 3000);
 	//middleware
 	app.use(express.static('./public'));
+	app.set('view engine', 'ejs');
+	app.set('views', './app/views');
+	home(app);
 	return app;
 };
