c1b88fbdc7893d0cecdd95538d2451b12091cb1c
doomsterinc <doomsterinc1@gmail.com>
2016-05-04 16:45:23 -0300

    addTesters

--- learn_angJS/js/controllers/controller.js
+++
@@ -1,6 +0,0 @@
-var myApp = angular.module('myApp', []);
-myApp.controller('MainCtr', ['$scope', function($scope) {
-	$scope.text = 'Testando!';
-}]);
-myapp.controller('NavCtrl', ['$scope', function($scope) {...} ]);
-myApp.controller('UserCtrl, ['$scope', function($scope) {...} ]);
--- testeionic/testeionic-master/www/js/controllers/chat-detail.controller.js
+++
@@ -1,7 +0,0 @@
-angular
-  .module('whatsapp')
-  .controller('ChatDetailCtrl', ChatDetailCtrl);
- 
-function ChatDetailCtrl ($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
-}
