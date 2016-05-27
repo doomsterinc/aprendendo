a157e9811b00d98c2b01262be79ff18e5ebcf09a
doomsterinc <doomsterinc1@gmail.com>
2016-05-27 15:36:34 -0300

    addchanges

---
+++ scripts/box.js
@@ -0,0 +1,12 @@
+var box = {};
+box.material = "cardboard";
+box.size = {
+	"height":2,
+	"width":80
+};
+
+box.area = function(){
+	return box.size.height * box.size.width;
+};
+
+console.log(box.area());
