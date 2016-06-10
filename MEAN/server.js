591db477f5f18d22806589b31b0e20b5649550c1
doomsterinc <doomsterinc1@gmail.com>
2016-06-10 17:03:33 -0300

    addChanges2

--- MEAN/server.js
+++ MEAN/server.js
@@ -2,15 +2,5 @@ var http = require('http');
 var app = require('./config/express')();
 
 http.createServer(app).listen(app.get('port'), function(){
-	console.log('Express Server escutando na porta' + app.get('port'));
+	console.log('Express Server escutando na porta ' + app.get('port'));
 });
-
-var box = {
-	"meon": "gato",
-	0 : "zero",
-	material: "cardboard"
-};
-
-for (var key in box){
-	console.log(box[key]);
-};
