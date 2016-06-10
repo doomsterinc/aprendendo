1c84635e66a8e8d8f1a700372c6ab35422ae5f33
doomsterinc <doomsterinc1@gmail.com>
2016-06-10 17:16:30 -0300

    addChanges3

--- MEAN/config/express.js
+++ MEAN/config/express.js
@@ -2,5 +2,9 @@ var express = require('express');
 
 module.exports = function() {
 	var app = express();
+	//settings environment
+	app.set('port', 3000);
+	//middlewares
+	app.use(express.static('./public'));
 	return app;
 };
