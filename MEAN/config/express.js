db536ba9c62c120b3d3a154af3854f5de29d9945
doomsterinc <doomsterinc1@gmail.com>
2016-06-11 10:53:13 -0300

    upviews

--- MEAN/config/express.js
+++ MEAN/config/express.js
@@ -6,5 +6,7 @@ module.exports = function() {
 	app.set('port', 3000);
 	//middlewares
 	app.use(express.static('./public'));
+	app.set('view engine', ejs);
+	app.set('views', './app/views');
 	return app;
 };
