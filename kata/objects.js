f7afc49927d322ad75e73dd3947aad0faae93531
doomsterinc <doomsterinc1@gmail.com>
2016-06-15 14:54:22 -0300

    addChanges

--- kata/objects.js
+++ kata/objects.js
@@ -30,21 +30,35 @@ var testname2 = new Name("Carol", "Diaz");
 console.log(testname2.fullName());
 
 var box = [];
-box[0] = true;
-box[1] = 'meow';
-box.push({hello:"hello", uou:"uou"});
+// box[0] = true;
+// box[1] = 'meow';
+// box.push({hello:"hello", uou:"uou"});
+// box['size'] = 9;
+// box['0'] = 'meow';
+// box.push("whoooooohoo!");
+// // var i = 0;
 
-var i = 0;
+// // // console.log(box[i]);
+// // // console.log(box[1]);
+// // // //console.log(box.pop());
+// // // console.log(box);
 
-console.log(box[i]);
-console.log(box[1]);
-//console.log(box.pop());
+// // //var box = [];
+
+// // for (var k in box){
+// // 	console.log(k);
+// // }
+
+// // // console.log(box);
+
+// for (var i = 0; i < box.length; i++){
+// 	console.log(box[i]);
+// }
+
+// console.log(box.size);
+
+box['0'] = "meow";
+box[3] = {'babyBox': true};
+
+console.log(box['length']);
 console.log(box);
-
-var box = [];
-
-box['size'] = 9;
-box['0'] = 'meow';
-
-console.log(box['size']);
-console.log(box[0]);
