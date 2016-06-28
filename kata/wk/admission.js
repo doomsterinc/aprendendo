7ed68302449d75b56f3db1e80a85dcac4801a2da
doomsterinc <doomsterinc1@gmail.com>
2016-06-28 16:06:28 -0300

    addChanges

---
+++ kata/wk/admission.js
@@ -0,0 +1,9 @@
+// Your code here
+var myArray = ["Maicon Giovani", ""];
+var cutName = function(word){
+  return word.split(" ");
+};
+var myData = {};
+myData.fullName = cutName("Maicon Giovani");
+myData.skype = myArray[1];
+myData.github = "doomsterinc";
