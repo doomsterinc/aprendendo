6e15b43822ac6f72bd7ef823cbf43ae800a5c3a5
doomsterinc <doomsterinc1@gmail.com>
2016-10-01 04:23:31 -0300

    cleaning

--- lessons/py/soma.py
+++
@@ -1,3 +0,0 @@
-a= input("escreva um numero: ")
-b= input("escreva outro numero: ")
-print (a+b)
--- lessons/testMEAN/app/routes/home.js
+++
@@ -1,6 +0,0 @@
-var controller = require('./app/controllers/homeController');
-
-module.exports = function(app) {
-	app.get('/index', controller.index);
-	app.get('/', controller.index);
-}
